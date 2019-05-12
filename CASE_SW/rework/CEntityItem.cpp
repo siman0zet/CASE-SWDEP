5f3a8b4ba6c85d0cbe4d8fb49f29df6e6e33fc0f
Victoria Mazmanyan <formclosemyth@gmail.com>
2019-05-12 21:53:16 +1000

    Entity item paint method changes

--- CASE_SW/rework/CEntityItem.cpp
+++ CASE_SW/rework/CEntityItem.cpp
@@ -5,7 +5,7 @@
 
 CEntityItem::CEntityItem() :
     _width(100),
-    _height(100)
+    _height(90)
 {
     this->setFlags(ItemIsSelectable | ItemIsMovable);
     this->setAcceptHoverEvents(true);
@@ -13,37 +13,69 @@ CEntityItem::CEntityItem() :
 
 QRectF CEntityItem::boundingRect() const
 {
-    return QRectF(-3, -3, _width + 6, _height + 6);
+    return QRectF(-5, -5, _width + 10 , _height + 10);
 }
 
 QPainterPath CEntityItem::shape() const
 {
     QPainterPath path;
-    path.addRoundedRect(0, 0, _width, _height, 5, 5);
+    path.addRect(0, 0, _width, _height);
     return path;
 }
 
 void CEntityItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *)
 {
+    prepareGeometryChange();
+
+    QPen oldPen = painter->pen();
+    QBrush oldBrush = painter->brush();
+
     if(option->state & QStyle::State_Selected)
     {
-        QBrush shadowBrush(QColor(192, 192, 192, 128), Qt::SolidPattern);
-        painter->setBrush(shadowBrush);
-        painter->setPen(Qt::NoPen);
-        painter->drawRoundedRect(-3, -3, _width + 6, _height + 6, 5, 5);
+        QPen selectPen(QBrush(QColor(Qt::gray)), 5);
+
+        painter->setPen(selectPen);
+        painter->setBrush(Qt::NoBrush);
+
+        painter->drawRect(0, 0, _width, _height);
     }
 
     QColor penColor = QColor(Qt::black);
     if(option->state & QStyle::State_Selected)
-        penColor = QColor(Qt::blue);
+        penColor = QColor(61, 141, 253);
     if(option->state & QStyle::State_MouseOver)
-        penColor = QColor(Qt::red);
-
-    QBrush fillBrush(Qt::white, Qt::SolidPattern);
-    QBrush penBrush(penColor, Qt::SolidPattern);
-    QPen pen(penBrush, 1);
-    painter->setBrush(fillBrush);
+        penColor = QColor(206, 70, 72);
+    QPen pen(penColor, 1);
+    QBrush brush(QColor(Qt::white));
     painter->setPen(pen);
+    painter->setBrush(brush);
 
-    painter->drawRoundedRect(0, 0, _width, _height, 5, 5);
+    painter->drawRect(-1, -1, _width + 2, _height + 2);
+
+    //content
+    QBrush titleBrush(QColor(225, 225, 225));
+    painter->setPen(Qt::NoPen);
+    painter->setBrush(titleBrush);
+
+    painter->drawRect(0, 0, _width, 30);
+
+    //title
+    painter->setPen(oldPen);
+    painter->setBrush(oldBrush);
+    QFont font("Calibri", 12);
+    font.setStyleStrategy(QFont::NoAntialias);
+    font.setBold(true);
+    painter->setFont(font);
+
+    painter->drawText(QRect(2, 5, _width - 2, 25), "table @");
+
+    //rows
+    font.setBold(false);
+    painter->setFont(font);
+    int textHeight = 30;
+    for(int i = 0; i < 3; i++)
+    {
+        painter->drawText(QRect(2, textHeight, _width + 2, 20), QString("[ ] row %1").arg(i));
+        textHeight += 20;
+    }
 }
